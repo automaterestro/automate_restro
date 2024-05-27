e385551019c4ab7e22bdb62a53632c112520eb1d
automaterestro <Utkarshsaxena0801@gmail.com>
2024-05-27 08:43:24 +0000

    modified:   app/manager/page.tsx

--- app/manager/page.tsx
+++ app/manager/page.tsx
@@ -19,7 +19,7 @@ const orders: Order[] = [
         waiterName: "John",
     },
     {
-        itemName: "Pizza",
+        itemName: "Pizza Sandwich",
         count: 1,
         rate: 250,
         totalAmt: 250,
@@ -28,7 +28,7 @@ const orders: Order[] = [
         waiterName: "Doe",
     },
     {
-        itemName: "Pizza",
+        itemName: "Cheese Maggie",
         count: 1,
         rate: 250,
         totalAmt: 250,
@@ -37,7 +37,16 @@ const orders: Order[] = [
         waiterName: "Doe",
     },
     {
-        itemName: "Pizza",
+        itemName: "Samosa",
+        count: 1,
+        rate: 250,
+        totalAmt: 250,
+        time: "12:45 PM",
+        tableNo: 3,
+        waiterName: "Doe",
+    },
+    {
+        itemName: "Garlic Nan",
         count: 1,
         rate: 250,
         totalAmt: 250,
@@ -50,48 +59,47 @@ const orders: Order[] = [
 export default function Manager() {
     return (
         <div className="flex flex-col gap-4 justify-center items-center px-4">
-            <h1>Dashboard</h1>
-            <section className="grid grid-cols-2 gap-4 w-full px-4 py-4">
-                <div className="grid grid-cols-1 col-span-1 gap-4 w-full">
+            <section className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full px-4 py-4 border">
+                <div className="col-span-1 flex flex-col gap-2 w-full">
                     <h1 className="text-red-500 font-semibold">Recent Orders</h1>
                     {orders.map((order, index) => (
                         <div key={index} className="flex flex-col gap-2 p-4 border-2">
                             <div className="flex justify-between flex-wrap">
-                                <span>Item: {order.itemName}</span>
-                                <span>Count: {order.count}</span>
-                                <span>Rate: ₹{order.rate}</span>
-                                <span>Total Amount: ₹{order.totalAmt}</span>
-                                <span>Time Served: {order.time}</span>
+                                <span className="text-red-500 font-semibold">{order.itemName}</span>
+                                <span>{order.count}</span>
+                                <span>₹{order.rate}</span>
+                                <span>Total: ₹{order.totalAmt}</span>
+                                <span>Time: {order.time}</span>
                                 <span>Table No: {order.tableNo}</span>
-                                <span>Waiter Name: {order.waiterName}</span>
+                                <span>Waiter: {order.waiterName}</span>
                             </div>
                         </div>
                     ))}
                 </div>
-                <div className="grid grid-cols-3 col-span-1 gap-2">
+                <div className="col-span-1 grid grid-cols-3 gap-2">
                     <div className="col-span-1 flex flex-col h-full items-center justify-center border-2">
                         <p className="text-2xl font-semibold">89</p>
-                        <p>No. of Customers</p>
+                        <p className="text-center">No. of Customers</p>
                     </div>
                     <div className="col-span-1 flex flex-col h-full items-center justify-center border-2">
                         <p className="text-2xl font-semibold">6</p>
-                        <p>No. of Chefs</p>
+                        <p className="text-center">No. of Chefs</p>
                     </div>
                     <div className="col-span-1 flex flex-col h-full items-center justify-center border-2">
                         <p className="text-2xl font-semibold">4</p>
-                        <p>No. of Serving Staff</p>
+                        <p className="text-center">No. of Serving Staff</p>
                     </div>
                     <div className="col-span-1 flex flex-col h-full items-center justify-center border-2">
                         <p className="text-2xl font-semibold">14</p>
-                        <p>No. of Employees</p>
+                        <p className="text-center">No. of Employees</p>
                     </div>
                     <div className="col-span-1 flex flex-col h-full items-center justify-center border-2">
                         <p className="text-2xl font-semibold">25</p>
-                        <p>No. of Food Items</p>
+                        <p className="text-center">No. of Food Items</p>
                     </div>
                     <div className="col-span-1 flex flex-col h-full items-center justify-center border-2">
                         <p className="text-2xl font-semibold">126</p>
-                        <p>No. of Orders</p>
+                        <p className="text-center">No. of Orders</p>
                     </div>
                 </div>
             </section>
